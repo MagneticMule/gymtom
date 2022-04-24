ceebc9c67b43050a1273b9222f4056db4321c6d4
Tommy <skywriter@gmail.com>
2022-04-24 21:05:46 +0100

    feat: :alembic: Added animations to signup form. Sexy.

--- components/TextSlider.tsx
+++ components/TextSlider.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {Animated, View, Text, StyleSheet} from 'react-native';
 import Slider from '@react-native-community/slider';
-import {useEffect, useState} from 'react';
+import {useState, useRef} from 'react';
 
 const defaultLevels = [
   'not important',
@@ -12,6 +12,26 @@ const defaultLevels = [
 ];
 
 const TextSlider = ({levels = defaultLevels, labelText}, ...args) => {
+  // textSpring will be used as the value for text size. Initial Value: 12
+  const springVal = useRef(new Animated.Value(1)).current;
+
+  const springIn = () => {
+    springVal.setValue(1);
+    Animated.timing(springVal, {
+      toValue: 1.5,
+      duration: 90,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const springOut = () => {
+    springVal.setValue(1.5);
+    Animated.spring(springVal, {
+      toValue: 1,
+      friction: 2,
+      useNativeDriver: true,
+    }).start();
+  };
   const [sliderVal, setSliderVal] = useState(levels[0]);
   const handleSliderChange = (value: number) => {
     setSliderVal(levels[value]);
@@ -21,7 +41,11 @@ const TextSlider = ({levels = defaultLevels, labelText}, ...args) => {
     <>
       <View style={styles.sliderContainer}>
         <Text style={styles.label}>
-          {labelText} is {sliderVal}
+          {labelText} is{'   '}
+          <Animated.View
+            style={[{transform: [{scale: springVal}]}, styles.levelContainer]}>
+            {sliderVal}
+          </Animated.View>
         </Text>
         <Slider
           style={{height: 40}}
@@ -31,6 +55,8 @@ const TextSlider = ({levels = defaultLevels, labelText}, ...args) => {
           minimumTrackTintColor="#FFFFFF"
           maximumTrackTintColor="#000000"
           onValueChange={value => handleSliderChange(value)}
+          onSlidingStart={value => springIn()}
+          onSlidingComplete={value => springOut()}
         />
       </View>
     </>
@@ -38,6 +64,12 @@ const TextSlider = ({levels = defaultLevels, labelText}, ...args) => {
 };
 
 const styles = StyleSheet.create({
+  levelContainer: {
+    backgroundColor: 'red',
+    color: 'white',
+    padding: 8,
+    borderRadius: 8,
+  },
   sliderContainer: {},
   label: {},
 });
